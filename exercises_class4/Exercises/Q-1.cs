b8afc5f01fe6b2bfc148e22ee3599477c977ef16
Madelin Dayana Vasquez Ramos <22773@belvedere.com>
2022-02-17 16:00:42 +0000

    Add project files.

---
+++ exercises_class4/Exercises/Q-1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exercises_class4.Exercises
+{
+    class Q_1
+    {
+        public void Multiplication()
+        {
+            Console.Write("Enter an integer : ");
+            string input = Console.ReadLine();
+            int num;
+            int.TryParse(input, out num); // returns num = 0 if input invalid
+            Console.WriteLine();
+            multiplicationTable(num);
+            Console.ReadKey();
+        }
+
+        static void multiplicationTable(int num)
+        {
+            for (int i = 2; i <= 10; i++)
+            {
+                Console.WriteLine("{0} x {1, -2} = {2}", num, i, num * i);
+            }
+        }
+    }
+}
---
+++ exercises_class4/Exercises/Q-2.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exercises_class4.Exercises
+{
+    class Q_2
+    {
+    }
+}
